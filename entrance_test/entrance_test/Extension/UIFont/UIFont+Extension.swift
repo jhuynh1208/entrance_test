00fe298697953bfd866972818c8c365eea001d03
Thiện <>
2022-11-26 22:00:36 +0700

    - add font + icon

---
+++ entrance_test/entrance_test/Extension/UIFont/UIFont+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIFont+Extension.swift
+//  entrance_test
+//
+//  Created by Thiện on 26/11/2022.
+//
+
+import UIKit
+
+extension UIFont {
+    static func montserratRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: "Montserrat-Regular", size: size)!
+    }
+    
+    static func montserratMedium(size: CGFloat) -> UIFont {
+        return UIFont(name: "Montserrat-Medium", size: size)!
+    }
+}
